82c4e478329ad882c6a226f6ff958f6ad68b49cf
RichardlWilson <richard.l.wilson85@gmail.com>
2021-05-24 16:41:49 -0500

    simple update file

--- black_jack_card_game.py
+++ black_jack_card_game.py
@@ -5,8 +5,39 @@
   #Value
   #Suit
   #Name
+suits = ('hearts', 'spades', 'diamonds', 'clubs')
+card_values = {
+    'two': 2 , 
+    'three' : 3,
+    'four' : 4,
+    'five' : 5,
+    'six' : 6,
+    'seven' : 7,
+    'eight' : 8,
+    'nine' : 9,
+    'ten' : 10,
+    'jack' : 10,
+    'queen' : 10,
+    'king' : 10,
+    'ace' : 11
+}
+
+class Cards:
+    def __init__(self, name, value, suit):
+        self.name =  
+        self.value = 
+        self.suit = 
+
+    def __str__(self):
+        return self.name + ' of ' + self.suit     
 
 #Deck
+
+class Deck:
+    def __init__(self):
+        for x in range(52):
+            for suit in suits:
+                for card in card_values
   # class to create all the cards
   #script to shuffle cards
 

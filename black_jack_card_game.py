e83f154cac7ee7b1e915a5d9cae5726cb255e07e
RichardlWilson <richard.l.wilson85@gmail.com>
2021-05-24 15:20:21 -0500

    build first file

---
+++ black_jack_card_game.py
@@ -0,0 +1,37 @@
+#Black Jack Card Game
+
+#Create:
+#Cards
+  #Value
+  #Suit
+  #Name
+
+#Deck
+  # class to create all the cards
+  #script to shuffle cards
+
+#player
+  #name
+  #player cards
+  #chip ammount
+
+#bank
+  #Players name
+  #chip amount  
+
+#menus
+  #title
+  #menu options
+
+
+
+#game Logic
+  #create deck of cards
+  #assign player and dealer
+  #deal cards
+    #menu options (hit, stay)
+    #dealer hand
+    #compare agains win or bust
+
+
+

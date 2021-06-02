d0302588ef33b0967f2a6abb7fd73259ea3f16db
RichardlWilson <richard.l.wilson85@gmail.com>
2021-06-02 16:32:26 -0500

    edited some code to meet pep8

--- black_jack_card_game.py
+++ black_jack_card_game.py
@@ -1,7 +1,10 @@
 #Black Jack Card Game
+'''
+Main file for Blackjack Game.
+'''
 
 from random import shuffle
-from os import name, system
+from os import system
 import time
 import graphics
 
@@ -25,10 +28,10 @@ class Card:
         self.suit = suit
 
     def __str__(self):
-        return self.rank + ' of ' + self.suit + f' ({self.value})'  
+        return self.rank + ' of ' + self.suit + f' ({self.value})'
 
     def __int__(self):
-        return self.value       
+        return self.value
 
 
 class Deck:
@@ -43,17 +46,22 @@ class Deck:
                 created_card = Card(rank, suit)
                 self.cards.append(created_card)
 
-    
     def __len__(self):
         return len(self.cards)
-    
+
 
     def shuffle(self):
+        '''
+        Shuffles the deck of cards.
+        '''
         shuffle(self.cards)
 
 
     def deal(self):
-        return self.cards.pop(0)    
+        '''
+        Deals one card
+        '''
+        return self.cards.pop(0)
 
 
 
@@ -65,10 +73,13 @@ class Dealer:
         self.pot = 0
         self.name = name
         self.cards = []
-        self.card_deck = Deck() 
+        self.card_deck = Deck()
 
 
     def sum_cards(self):
+        '''
+        Returns the sum of all cards in dealer's hand
+        '''
         total = 0
         for card in self.cards:
             total += card.value
@@ -77,15 +88,24 @@ class Dealer:
 
 
     def sum_card_one(self):
-        return dealer.cards[0].value
+        '''
+        Returns the first card's value in the dealers hand.
+        '''
+        return self.cards[0].value
 
 
     def show_cards_one(self):
+        '''
+        Returns the suit and rank of the first card in the dealer's hand
+        '''
         return str(self.cards[0])
 
 
-    def show_cards_all(self):    
-        return ', '.join(str(card).title() for card in self.cards) 
+    def show_cards_all(self):
+        '''
+        Returns the suit and rank of all the cards in the dealer's hand.
+        '''
+        return ', '.join(str(card).title() for card in self.cards)
 
 
 
@@ -108,19 +128,25 @@ class Player:
         user_input = ''
         while len(user_input) <1:
             user_input = input(' '*42 + 'Enter Name: ')
-            self.name = user_input    
-        
+            self.name = user_input
+
 
     def sum_cards(self):
+        '''
+        Returns the sum of all the cards in the player's hand.
+        '''
         total = 0
         for card in self.cards:
             total += card.value
 
-        return total           
+        return total
 
 
     def show_cards_all(self):
-        return ', '.join(str(card).title() for card in self.cards)        
+        '''
+        Returns the suit and rank of all the cards in the player's hand
+        '''
+        return ', '.join(str(card).title() for card in self.cards)
 
 
 
@@ -132,7 +158,7 @@ def main_menu():
     menu = '    1. Start Game \n    2. Exit\n'
 
     print(menu)
-    
+
     user_input = 0
     while user_input not in options:
         user_input = input('    Enter the number of the option: ')
@@ -151,10 +177,10 @@ def main_menu():
         graphics.title()
         goodbye()
 
-            
+
 def goodbye():
     '''
-    game exit code.
+    Game exit code.
     '''
     print(' '*34 + 'Thank you for playing!!!!!!')
     time.sleep(2)
@@ -165,7 +191,7 @@ def goodbye():
 if __name__ == '__main__':
     system('mode 94, 45')
     system('color 0A')
-    
+
     game_on = True
     while game_on:
         #BUILDING THE GAME
@@ -173,34 +199,34 @@ if __name__ == '__main__':
         dealer.card_deck.shuffle()
         player = Player()
         match = 0
-        
+
         #STARTING THE GAME
         graphics.title()
         main_menu()
-        
+
         #NAMING THE PLAYER
         graphics.title()
         player.new_name()
 
         #STARTING THE MATCH
         graphics.title()
-        
+
 
         user_input = ''
-        while user_input != 'd' and user_input != 'q':
+        while user_input not in ('d', 'q'):
             name_len = len(player.name)//2
             print(' '*(32-name_len) + f'Hello {player.name} are you ready to play?\n')
-            user_input = input(' '*37 + f'(D) Deal or (Q) Quit\n').lower()
+            user_input = input(' '*37 + '(D) Deal or (Q) Quit\n').lower()
 
 
         if user_input == 'q':
             graphics.title()
             goodbye()
-        
+
 
         match_on = True
         while match_on:
-            
+
             #DEALING CARDS
             for _ in range(2):
                 player.cards.append(dealer.card_deck.deal())
@@ -211,7 +237,7 @@ if __name__ == '__main__':
             graphics.title()
             graphics.game_info_bar(player.name, player.chips, match)
             graphics.show_hands(dealer.cards, player.cards, player.name)
-            
+
 
             #ENTERING BET
             while True:
@@ -219,19 +245,19 @@ if __name__ == '__main__':
 
                 try:
                     player.bet_chips = int(player.bet_chips)
-                    pass
+
                 except:
                     graphics.title()
                     graphics.game_info_bar(player.name, player.chips, match)
                     graphics.show_hands(dealer.cards, player.cards, player.name)
 
                     print('Error! Please enter a number.')
-                    continue    
+                    continue
 
                 if player.chips >= player.bet_chips:
                     player.chips -= player.bet_chips
                     break
-                else:    
+                else:
                     print('you dont\'t have enough chips! Try a lower amount.')
                     continue
 
@@ -242,24 +268,25 @@ if __name__ == '__main__':
                 graphics.game_info_bar(player.name, player.chips, match)
                 graphics.show_hands(dealer.cards, player.cards, player.name)
 
-                
+
                 #CHOOSE THE ACE VALUE
                 while True:
                     for card in player.cards:
                         if card.value ==0:
-                            while user_input != 'a' and user_input != 'b':
-                                user_input = input('For Ace, Value (A) [1] or (B) [11]').lower()
+                            while user_input not in ('a', 'b'):
+                                user_input = input(' '*30 + 'For Ace, Value (A)' \
+                                    + '[1] or (B) [11]').lower()
 
                             if user_input == 'a':
                                 card.value = 1
                             else:
-                                card.value = 11 
+                                card.value = 11
 
                     graphics.title()
                     graphics.game_info_bar(player.name, player.chips, match)
-                    graphics.show_hands(dealer.cards, player.cards, player.name)            
-                    break          
-                
+                    graphics.show_hands(dealer.cards, player.cards, player.name)
+                    break
+
 
                 #CHECK FOR BUST
                 if player.sum_cards() > 21:
@@ -274,7 +301,7 @@ if __name__ == '__main__':
                     break
 
                 user_input = ''
-                while user_input != 'h' and user_input != 's' and user_input != 'q':
+                while user_input not in ('h', 's', 'q'):
                     user_input = input(' '*30 + '(H) Hit  (S) Stand        (Q) Quit\n').lower()
 
                 if user_input == 'h':
@@ -284,8 +311,8 @@ if __name__ == '__main__':
                     match_on = False
                     break
                 elif user_input == 's':
-                    break    
-                
+                    break
+
 
             #DEALER FINISHES LAYING DOWN CARDS
             while dealer.sum_cards() < 17:
@@ -302,10 +329,10 @@ if __name__ == '__main__':
             #CHECK FOR WIN OR LOSE
             if dealer.sum_cards() > 21 and player.sum_cards() <= 21 or \
                 player.sum_cards() > dealer.sum_cards() and player.sum_cards() <=21:
-                
+
                 player.chips += player.bet_chips*2
                 player.bet_chips = 0
-                
+
                 graphics.title()
                 graphics.game_info_bar(player.name, player.chips, match)
                 graphics.show_hands(dealer.cards, player.cards, player.name)
@@ -324,7 +351,7 @@ if __name__ == '__main__':
 
                 graphics.lose()
                 input()
-            
+
 
             #CHECK IF OUT OF CHIPS
             if player.chips == 0:
@@ -337,7 +364,7 @@ if __name__ == '__main__':
             user_input = ''
             while user_input != 'y' and user_input != 'n':
                 user_input = input(' '*32 + 'play again? (Y) Yes or (N) No\n').lower()
-            
+
             if user_input == 'y':
                 player.cards.clear()
                 dealer.cards.clear()
